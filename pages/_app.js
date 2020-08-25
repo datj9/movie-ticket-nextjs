e9c80d291c7fcc4b089dae1138d4b491bfcba1ff
tandat198 <tandat198@gmail.com>
2020-08-25 21:13:39 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import "../styles/globals.scss";
 import Header from "../components/header";
 import { wrapper } from "../redux/store";
-import { useDispatch } from "react-redux";
-import { logOut, signIn } from "../redux/user/actions";
 
 function MyApp({ Component, pageProps }) {
     return (
